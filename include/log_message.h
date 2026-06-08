a012b13c6d3845ff1575996c7a7d6475948d0306
ljt2 <3447794990@qq.com>
2026-06-08 21:20:32 +0800

    增加了成员变量

--- include/log_message.h
+++ include/log_message.h
@@ -15,5 +15,7 @@ namespace ljt
         std::string payload;                        // 有效载荷
         std::chrono::system_clock::time_point time; // 时刻[系统时间]
         std::thread::id tid;                        // 线程id
+        const char* source_file{""};                // 源文件名
+        int source_line{0};                         // 源文件行号
     };
 }
