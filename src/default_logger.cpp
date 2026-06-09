8363a0a5cf5bf32dff8b83149440f66d99f481d8
ljt2 <3447794990@qq.com>
2026-06-09 21:43:57 +0800

    默认输出器换成带颜色的

--- src/default_logger.cpp
+++ src/default_logger.cpp
@@ -1,11 +1,13 @@
 #include "../include/default_logger.h"
+#include "../include/sink/color_stdout_sink.h"
 
 namespace ljt
 {
     Logger& defaultLogger()
     {
         // C++11 保证静态局部变量初始化是线程安全的
-        static Logger logger("default", std::make_shared<StdoutSink>());
+        // 使用彩色输出到 stdout，更直观
+        static Logger logger("default", std::make_shared<ColorStdoutSink>());
         return logger;
     }
 }
